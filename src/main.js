513b4d9d26aa4af091b0cb1e3e9f5bda00aab487
Vitor Nogueira <vslnogueira96@gmail.com>
2022-11-16 16:48:52 -0300

    Resolvendo o requisito [4] do Projeto Shopping Cart

--- src/main.js
+++ src/main.js
@@ -5,6 +5,23 @@ import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const sectionPai = document.querySelector('.products');
+const sectionContainer = document.querySelector('.container');
 
+function addLoading() {
+  const loading = document.createElement('section');
+  loading.classList.add('loading');
+  loading.textContent = 'carregando...';
+  sectionContainer.appendChild(loading);
+}
+addLoading();
+
+function rmvLoading() {
+  const loading = document.querySelector('.loading');
+  loading.remove();
+}
 const listComputer = await fetchProductsList('computador');
-listComputer.map((element) => sectionPai.appendChild(createProductElement(element)));
+listComputer.map((element) => {
+  rmvLoading();
+  const addProd = sectionPai.appendChild(createProductElement(element));
+  return addProd;
+});
