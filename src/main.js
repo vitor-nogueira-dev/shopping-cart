4cd247d75febf31be62c51ec2804823f067dcf8b
Vitor Nogueira <vslnogueira96@gmail.com>
2022-11-16 16:23:43 -0300

    Resolvendo o requisito [3] do Projeto Shopping Cart

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const sectionPai = document.querySelector('.products');
+
+const listComputer = await fetchProductsList('computador');
+listComputer.map((element) => sectionPai.appendChild(createProductElement(element)));
