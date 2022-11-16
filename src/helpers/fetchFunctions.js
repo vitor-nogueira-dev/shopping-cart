728e089074e36e920cfb4dbd56f19a10d636b193
Vitor Nogueira <vslnogueira96@gmail.com>
2022-11-16 19:19:43 -0300

    Resolvendo os requisitos [6 e 7] do Projeto Shopping Cart

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,14 @@
-export const fetchProduct = () => {
-  // seu código aqui
+export const fetchProduct = async (idProduct) => {
+  if (!idProduct) {
+    throw new Error('ID não informado');
+  }
+  const URL_PRODUCT = `https://api.mercadolibre.com/items/${idProduct}`;
+  const promisse = await fetch(URL_PRODUCT);
+  const data = await promisse.json();
+  // console.log(data);
+  return data;
 };
-
+// await fetchProduct('MLB2961982059');
 export const fetchProductsList = async (categoria) => {
   if (categoria === undefined) {
     throw new Error('Termo de busca não informado');
--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -3,8 +3,24 @@ import { fetchProduct } from '../helpers/fetchFunctions';
 import product from './mocks/product';
 
 // implemente seus testes aqui
-describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+describe("Implementando testes para fetchProduct()", () => {
+  it('Verifica se fetch é uma função', () => {
+    expect(typeof fetchProduct).toBe('function');
   });
+  it('Verificando se fetchProduct MLB1405519561 recebendo o parâmetro computador foi chamada com a URL correta', async () => {
+    await fetchProduct('MLB1405519561')
+    expect(fetch).toHaveBeenCalled()
+  });
+  it('Verificando se a função fethProduct() com MLB1405519561 a função utiliza o endpoint', async () => {
+    const idProduct = 'MLB1405519561';
+    await fetchProduct('MLB1405519561')
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1405519561')
+  })
+  it('Testando se o retorno da função fetchProduct com o parâmetro MLB1405519561 é uma estrutura igual ao objeto ITEM', async () => {
+    const chamadaFuncao = await fetchProduct('MLB1405519561')
+    expect(chamadaFuncao).toEqual(product);
+  })
+  it('Testando se a função sem parâmetro retorna um erro', async () => {
+    await expect(fetchProduct()).rejects.toThrow(); // podemos testar colocando a frase ai dentro tbm 'ID não informado'
+  })
 });
